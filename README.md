2778323cb91b5e03e7e5ac3022a35ffb2a97212e
bubunyun <171100337+bubunyun@users.noreply.github.com>
2025-03-20 15:38:17 +0000

    add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Steps to update documentation
+
+```shell
+doxygen Doxyfile
+```
+
+`Doxyfile` points to the source code folder.
+
+```shell
+git commit -am "update"; git push
+```
+
+Commit and push the regenerated documentation.
---
+++ docs/classBuffer.js
@@ -0,0 +1,7 @@
+var classBuffer =
+[
+    [ "Buffer", "classBuffer.html#ae7ef2cd201190fde551dcb902627112b", null ],
+    [ "getAverage", "classBuffer.html#ae74bc34f36a001f37f2d8a0250ea29f2", null ],
+    [ "getAverageNoAnomalies", "classBuffer.html#add8dacf5e2a8b9f4ff4f4e2a915b7ff9", null ],
+    [ "put", "classBuffer.html#a8528d3203b5fe5f13bb5aeb8e547f3b5", null ]
+];
